8fe1ed056d53c0c566952f9fb47b3b1e2bffe04e
hungrybeagle <cyang@deltalearns.ca>
2023-02-22 09:29:44 -0800

    Create task4.py

---
+++ task4.py
@@ -0,0 +1,22 @@
+#!python3
+
+"""
+Strings are iterable.  Use for loops to iterate through both strings to create a list to represent a deck of cards. Note: We can also use list comprehension as strings are still iterable!
+"""
+
+ranks = "A23456789TJQK"
+suits = "CDHS"
+
+def createDeck():
+  return 
+
+def main():
+  deck = createDeck()
+  print(deck)
+  assert "AS" in deck
+  assert "KC" in deck
+  assert deck.count("TC") == 1
+
+
+if __name__ == "__main__":
+  main()
